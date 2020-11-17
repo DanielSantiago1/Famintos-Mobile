f6d7ffd1b01c7dc12eb85efe1fb9b4be62dec98c
Ericton <erictonbrito@outlook.com>
2020-11-16 21:46:23 -0400

    tela de cardapio, categoria e sobre
    Signed-off-by: Ericton <erictonbrito@outlook.com>

---
+++ famintos_mobile/lib/src/pages/listaCategoria/comboEspecial.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ComboEspecialPage extends StatefulWidget {
+  @override
+  _ComboEspecialPageState createState() => _ComboEspecialPageState();
+}
+
+class _ComboEspecialPageState extends State<ComboEspecialPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff6D4C41),
+        title: const Text('COMBO ESPECIAL'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
